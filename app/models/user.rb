6e3da7e7bf0dd9a16ac7654ae0e8ed422fd36d7a
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-04-14 00:14:30 +0900

    finish 8-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_many :likes
 
+  has_many :comments
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
